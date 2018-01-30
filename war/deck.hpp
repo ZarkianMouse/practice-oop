d133938c8370524a7d142c2733f1922cf6d66787
zarkianmouse <naomi.burhoe@hotmail.com>
2018-01-30 00:04:35 -0500

    Committing deck revisions

--- war/deck.hpp
+++ war/deck.hpp
@@ -14,11 +14,23 @@ class Deck
        deck.push_back(n);	    
     }
    
+    Card removeCard()
+    {
+       Card n = deck.back();
+       deck.pop_back();
+       return n;
+    }
+   
     std::vector<Card> copyDeck()
     {
        return deck;
     }
     
+    int size()
+    { 
+       return deck.size();
+    }
+
     void displayDeck()
     {
       for(unsigned int i = 0; i < deck.size(); ++i)
@@ -40,16 +52,29 @@ class Deck
       std::vector<Card> deck;
 };
 
-Deck generateDeck()
+Deck generateDeck(int size)
 {
-    Card newCard(Ace, Spades);
-    Deck myDeck;
-    myDeck.addCard(newCard);
+    Deck deck;
+    bool cardThere;
+    for(unsigned int i = 0; i < size; ++i)
+    {
+       Card n(static_cast<Rank>(rand() % End), static_cast<Suit>(rand() % Blank));
+       cardThere = deck.searchDeck(n);
+       if (!cardThere)
+          deck.addCard(n);
+       else
+          --i;
+    }
+    return deck;
+}
 
-    
-    Card newCard2(Ace, Spades);
-    bool cardThere = myDeck.searchDeck(newCard2);
-    if (cardThere)
-        std::cout << "true\n";
-
+void divideDeck(Deck orig, Deck &p1, Deck &p2)
+{
+   int i = orig.size() - 1;
+   while (i > 0)
+   {
+      p1.addCard(orig.removeCard());
+      p2.addCard(orig.removeCard());
+      i -= 2;
+   }
 }
--- war/main.cpp
+++ war/main.cpp
@@ -13,6 +13,8 @@
 //    - equal... flip some extra cards, recurse
 //    - winning player gets sacrifices (how?)
 
+const int SIZE = 52;
+
 int
  main() {
     std::cout << "hello world\n";
@@ -20,12 +22,17 @@ int
     test();
     std::cout << "3...2...1...War!!\n";
     
-    Deck myDeck = generateDeck();
+    Player p1("Player 1");
+    Player p2("Player 2");
+    std::cout << p1.getName() << " vs. " << p2.getName() << '\n';
     
-    std::cout << "deck shuffled\n";
-    //Loop
-    //generateDeck
-    //divideDeck
+    Deck myDeck = generateDeck(SIZE);
+    std::cout << "Deck generated\n";
+    
+    Deck deckP1;
+    Deck deckP2;
+    divideDeck(myDeck, deckP1, deckP2);
+    std::cout << "Decks divided\n";
 
     //gameLoop
     //revealCard1
