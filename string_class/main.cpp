19a7b758b26bdfe18ad9882cf81288e95a3ebaf6
zarkianmouse <naomi.burhoe@hotmail.com>
2018-02-01 09:50:29 -0500

    updating main.cpp

--- string_class/main.cpp
+++ string_class/main.cpp
@@ -4,5 +4,34 @@ int
 main()
 {
   std::cout << "hello world/n";
+
+/*
+  // Default construct
+string s0;
+
+// Construct and assign from a literal value
+string s1 = "hello";
+s1 = "foo";
+
+// Copy construct and assign
+string s2 = s1;
+s2 = s1; // s2 == s1
+std::cout << "s2 = " << s2 << '\n';
+
+// Move construction and assignment (optional)
+string s3 = std::move(s2);
+s3 = std::move(s1);
+
+// Compare
+s1 == s1;
+s2 == s1;
+
+// Order
+s1 < s1; // lexicographical comparison
+
+// Object independence
+s2 = "bar"; // Does not change the value of s1
+ 
+*/
   return 0;
 }
